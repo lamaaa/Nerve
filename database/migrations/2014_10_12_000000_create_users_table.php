bfc7c6b6d156ba52ae7c00206ad4a546c5a56c7c
root <root@DESKTOP-7VHAMG7.localdomain>
2018-03-10 13:58:58 +0800

    feat: 完成用户注册与登录

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('username')->unique()->default('')->comment('用户名');
+            $table->string('phone')->unique()->default('')->comment('手机号码');
+            $table->string('email')->unique()->default('')->comment('邮箱');
+            $table->string('password')->default('')->comment('密码哈希');
+            $table->smallInteger('status')->default(1)->comment('状态');
             $table->rememberToken();
             $table->timestamps();
         });
