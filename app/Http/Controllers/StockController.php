b0d8b4a68d7c0ecf8c4d4d7d497f6248e9895419
lamaaa <haoyang.lin@foxmail.com>
2018-03-20 20:30:29 +0800

    feat: 1.为StockControler增加auth中间件 2.删除无用代码

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -13,31 +13,8 @@ class StockController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth');
         $this->redisPrefix = Config::get('database.redis.default.prefix');
-    }
-
-    public function test(Request $request)
-    {
-        $page = $request->input('page', 1);
-        $stockPaginatorArray = Stock::where(['stock_status' => Stock::NORMAL])->paginate(16, ['*'], 'page', $page)->toArray();
-        $stockQuotesArray = Redis::pipeline(function ($pipe) use ($stockPaginatorArray) {
-            foreach ($stockPaginatorArray['data'] as $stock) {
-                $pipe->hgetall($this->redisPrefix . 'stocks:' . $stock['exchange'] . $stock['code']);
-            }
-        });
-        unset($stockPaginatorArray['data']);
-        unset($stockPaginatorArray['next_page_url']);
-        unset($stockPaginatorArray['prev_page_url']);
-        unset($stockPaginatorArray['path']);
-        $stockPaginatorArray['stocks'] = $stockQuotesArray;
-        return response()->json(['data' => $stockPaginatorArray], 200);
-
-//        $stockDataArray = Redis::pipeline(function ($pipe) use ($stocks, $redisPrefix) {
-//            foreach ($stocks as $stock) {
-//                $pipe->hgetall($redisPrefix . $stock->exchange . $stock->code);
-//            }
-//        });
-//        return response()->json(['data' => $stockDataArray], 200);
     }
 
     public function index(Request $request)
